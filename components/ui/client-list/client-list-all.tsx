7d3fe6e8334be3652bc9a9c41b55c9d48d7b9702
Patrick MacDonald <patrick@patmac.ca>
2025-07-17 20:36:15 -0600

    added feilds

--- components/ui/client-list/client-list-all.tsx
+++ components/ui/client-list/client-list-all.tsx
@@ -7,18 +7,17 @@ import { Client } from "../../../types/types";
 export default async function ClientListAll() {
     const clients: Client[] = await FetchAllClients();
     return (
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<ContentContainer>Loading...</ContentContainer>}>
             <ul className="flex flex-col gap-4 rounded-sm w-full items-center">
                 {clients.map(client => (
                     <ContentContainer key={client.id}>
-                        <li
-                            className="border p-4 rounded-sm"
-                        >
+                        <li className="border p-4 rounded-sm">
                             <p>Name: {client.fullname}</p>
                             <p>Email: {client.emailaddress}</p>
                             <p>Address: {client.address}</p>
-                            <MapComponent address={client.address} />
+                            <p>Maintenance Week{client.maintenanceweek}</p>
                             <p>Amount owing: ${client.amountowing} </p>
+                            <MapComponent address={client.address} />
                         </li>
                     </ContentContainer>
                 ))}
--- types/types.ts
+++ types/types.ts
@@ -4,4 +4,5 @@ export interface Client {
     emailaddress: string;
     address: string;
     amountowing: string;
+    maintenanceweek:number;
 }
