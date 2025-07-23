50e6e7fc3d57a0dff56482966b1c2fcba54b2fbc
Patrick MacDonald <patrick@patmac.ca>
2025-07-22 20:53:45 -0600

    added fall back

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -4,13 +4,16 @@ import { InputField } from "@/components/ui/inputs/input";
 import PricePerCutInput from "@/components/ui/settings/price-per-cut-input";
 import { SettingsLabel } from "@/components/ui/settings/settings-label";
 import { FetchPricePerCut } from "@/DAL/dal";
+import { Suspense } from "react";
 
 export default function Settings() {
     const pricePerCutePromise = FetchPricePerCut();
     return (
         <ContentContainer>
             <h1>Settings</h1>
-            <PricePerCutInput pricePerCutPromise={pricePerCutePromise} />
+            <Suspense fallback={ <InputDiv ><SettingsLabel  text="Price Per Cut"/><InputField id={"price_pre_cut"} name={"price_pre_cut"} type={"number"} step="0.01" defaultValue={0.00} /></InputDiv>}>
+                <PricePerCutInput pricePerCutPromise={pricePerCutePromise} />
+            </Suspense>
             <InputDiv >
                 <SettingsLabel text={"Stripe API Key"} />
                 <InputField id={"stripe_api_key"} name={"stripe_api_key"} type={"text"} placeholder={"Your Stripe API Key"} />
