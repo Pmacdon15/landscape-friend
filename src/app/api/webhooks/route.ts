83d950db15a12fef8d86214f83efbafab08554db
Patrick MacDonald <patrick@patmac.ca>
2025-07-22 18:57:03 -0600

    test

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,20 +1,49 @@
-import { handleOrganizationCreated, handleSubscriptionUpdate } from '@/lib/functions';
+import { clerkClient } from '@clerk/nextjs/server';
 import { verifyWebhook } from '@clerk/nextjs/webhooks'
 import { NextRequest } from 'next/server'
-import { SubscriptionItem, OrganizationCreatedEvent, WebhookEvent } from '@/types/types'
 
-function isSubscriptionItem(data: WebhookEvent['data']): data is SubscriptionItem {
+function isSubscriptionItem(data: any): data is { plan: { slug: string } } {
     return 'plan' in data && 'slug' in data.plan;
 }
 
 export async function POST(req: NextRequest) {
     try {
-        const evt = await verifyWebhook(req, {
+        const evt: any = await verifyWebhook(req, {
             signingSecret: process.env.CLERK_WEBHOOK_SIGNING_SECRET,
-        }) 
+        })
 
-        console.log("Webhook Type: ", evt.type)
-      
+        console.log(`Received webhook with event type: ${evt.type}`)
+        console.log('Webhook payload:', evt.data)
+        console.log('organization_id:', evt.data.payer?.organization_id)
+        console.log("plan: ", evt.data.plan?.slug)
+
+        if (evt.type === 'subscriptionItem.active') {
+            if (isSubscriptionItem(evt.data)) {
+                const plan = evt.data.plan.slug;
+                const orgId = evt.data.payer?.organization_id
+
+                if (orgId) {
+                    const clerk = await clerkClient();
+
+                    if (plan === 'basic_10_people_org') {
+                        await clerk.organizations.updateOrganization(orgId, {
+                            maxAllowedMemberships: 10
+                        });
+                    }
+                    else if (plan === 'pro_25_people_org') {
+                        await clerk.organizations.updateOrganization(orgId, {
+                            maxAllowedMemberships: 25
+                        })
+                    } else {
+                        await clerk.organizations.updateOrganization(orgId, {
+                            maxAllowedMemberships: 1
+                        })
+                    }
+                }
+            }
+
+        }
+
         return new Response('Webhook received', { status: 200 })
     } catch (err) {
         if (err instanceof Error) {
