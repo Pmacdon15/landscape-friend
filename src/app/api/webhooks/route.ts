7564be9cc507415bdfc0c32152bc947c7c7638bd
Patrick MacDonald <patrick@patmac.ca>
2025-07-22 19:04:58 -0600

    test

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,6 +1,7 @@
-import { clerkClient } from '@clerk/nextjs/server';
+import { clerkClient, WebhookEvent } from '@clerk/nextjs/server';
 import { verifyWebhook } from '@clerk/nextjs/webhooks'
 import { NextRequest } from 'next/server'
+
 
 function isSubscriptionItem(data: any): data is { plan: { slug: string } } {
     return 'plan' in data && 'slug' in data.plan;
@@ -8,41 +9,41 @@ function isSubscriptionItem(data: any): data is { plan: { slug: string } } {
 
 export async function POST(req: NextRequest) {
     try {
-        const evt: any = await verifyWebhook(req, {
+        const evt: WebhookEvent = await verifyWebhook(req, {
             signingSecret: process.env.CLERK_WEBHOOK_SIGNING_SECRET,
         })
 
         console.log(`Received webhook with event type: ${evt.type}`)
         console.log('Webhook payload:', evt.data)
-        console.log('organization_id:', evt.data.payer?.organization_id)
-        console.log("plan: ", evt.data.plan?.slug)
+        // console.log('organization_id:', evt.data.payer?.organization_id)
+        // console.log("plan: ", evt.data.plan?.slug)
 
-        if (evt.type === 'subscriptionItem.active') {
-            if (isSubscriptionItem(evt.data)) {
-                const plan = evt.data.plan.slug;
-                const orgId = evt.data.payer?.organization_id
+        // if (evt.type === 'subscriptionItem.active') {
+        //     if (isSubscriptionItem(evt.data)) {
+        //         const plan = evt.data.plan.slug;
+        //         const orgId = evt.data.payer?.organization_id
 
-                if (orgId) {
-                    const clerk = await clerkClient();
+        //         if (orgId) {
+        //             const clerk = await clerkClient();
 
-                    if (plan === 'basic_10_people_org') {
-                        await clerk.organizations.updateOrganization(orgId, {
-                            maxAllowedMemberships: 10
-                        });
-                    }
-                    else if (plan === 'pro_25_people_org') {
-                        await clerk.organizations.updateOrganization(orgId, {
-                            maxAllowedMemberships: 25
-                        })
-                    } else {
-                        await clerk.organizations.updateOrganization(orgId, {
-                            maxAllowedMemberships: 1
-                        })
-                    }
-                }
-            }
+        //             if (plan === 'basic_10_people_org') {
+        //                 await clerk.organizations.updateOrganization(orgId, {
+        //                     maxAllowedMemberships: 10
+        //                 });
+        //             }
+        //             else if (plan === 'pro_25_people_org') {
+        //                 await clerk.organizations.updateOrganization(orgId, {
+        //                     maxAllowedMemberships: 25
+        //                 })
+        //             } else {
+        //                 await clerk.organizations.updateOrganization(orgId, {
+        //                     maxAllowedMemberships: 1
+        //                 })
+        //             }
+        //         }
+        //     }
 
-        }
+        // }
 
         return new Response('Webhook received', { status: 200 })
     } catch (err) {
