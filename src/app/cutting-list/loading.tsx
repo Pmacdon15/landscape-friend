e5d2e8a34c0a5bed5097d4103aad33c84afaffb1
Patrick MacDonald <patrick@patmac.ca>
2025-07-27 15:31:05 -0600

    fixed build errors

--- src/app/cutting-list/loading.tsx
+++ src/app/cutting-list/loading.tsx
@@ -1,6 +1,7 @@
 import SearchForm from "@/components/ui/client-list/search-form";
 import ContentContainer from "@/components/ui/containers/content-container";
 import HeaderWithSearch from "@/components/ui/containers/header-with-search";
+import { Suspense } from "react";
 
 export default function Loading() {
     return (
@@ -8,7 +9,9 @@ export default function Loading() {
             <ContentContainer>
                 <HeaderWithSearch>
                     <h1 className="text-2xl">Client Cutting List</h1>
-                    <SearchForm isCuttingDayComponent={true} />
+                    <Suspense>
+                        <SearchForm isCuttingDayComponent={true} />
+                    </Suspense>
                 </ HeaderWithSearch>
             </ContentContainer>
         </>
