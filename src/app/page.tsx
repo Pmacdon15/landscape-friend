250aad203c37c16ede1a9b44d66a0615f9be504c
Patrick MacDonald <patrick@patmac.ca>
2025-08-02 19:19:14 -0600

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <FormContainer>
       <ContentContainer>
-        <h1 className="text-3xl font-bold mb-6">Welcome to LawnBuddy</h1>
-        <p className="mb-4">
+        <h1 className="text-3xl font-bold mb-6">Welcome to Lawn Buddy</h1>
+        <p className="mb-4 indent-4">
           Thank you for choosing LawnBuddy! To learn more about how we work and protect your data, please review our{" "}
           <Link href="/terms-of-service" className="text-blue-600 underline">
             Terms of Service
