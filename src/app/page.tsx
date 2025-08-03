406cb0d09ab86286d574d6ed94b6293b4bada768
Luis-Sy <74744192+Luis-Sy@users.noreply.github.com>
2025-08-03 17:10:19 -0600

    initial animations for landing page logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,37 @@
 import ContentContainer from "@/components/ui/containers/content-container";
 import FormContainer from "@/components/ui/containers/form-container";
 import Link from "next/link";
+import "@/app/animations/landing-page.css";
+
 
 export default function Home() {
   return (
+    <> 
+
+    <div className="flex justify-center items-center mb-4 overflow-hidden width-100% md: overflow-visible">
+
+      <div className="flex justify-right items-right logo-slide-in">
+        <img
+            src="/logo.png"
+            alt="Lawn Buddy Logo"
+            className="w-64 h-auto mx-auto mb-4 "
+          />
+        
+      </div>
+
+      <div className="flex-columns justify-top items-left h-16 bg-green-500 text-white logo-slide-in-reverse">
+        <h1 className="text-3xl font-bold">Lawn Buddy</h1>
+        <div className="fit bg-green-700">
+          <h2 className="text-xl font-semibold">Your Lawn Care Companion</h2>
+        </div>
+        
+      </div>
+
+    </div>
+      
     <FormContainer>
       <ContentContainer>
+        
         <h1 className="text-3xl font-bold mb-6">Welcome to Lawn Buddy</h1>
         <p className="mb-4 indent-4">
           Thank you for choosing LawnBuddy! To learn more about how we work and protect your data, please review our{" "}
@@ -20,5 +46,6 @@ export default function Home() {
         </p>
       </ContentContainer>
     </FormContainer>
+    </>
   );
 }
