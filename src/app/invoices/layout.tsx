33a7808f5bcc4bcde6c50ef399424048d1c2a018
Patrick MacDonald <patrick@patmac.ca>
2025-08-10 15:32:22 -0600

    code clean up and set up error.tsx

---
+++ src/app/invoices/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Landscape Friend',
+  description: 'Track and Invoice your lawn clients easily',
+  keywords: 'lawn care, lawn tracking, invoice lawn clients, lawn management, send quote, manage invoices, manage quotes ',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,32 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
-export default clerkMiddleware()
+const isProtectedRoute = createRouteMatcher(['/lists(.*)', '/(email)(.*)'])
+const isAdminRoute = createRouteMatcher(['/invoices(.*)'])
 
+export default clerkMiddleware(async (auth, req) => {
+  // const clerk = await clerkClient()
+  const { userId, orgId, sessionClaims } = await auth.protect()
+  // console.log("Admin?: ", sessionClaims?.orgRole)
+  if (isAdminRoute(req) && sessionClaims?.orgRole !== 'org:admin' && orgId) {
+    const url = req.nextUrl.clone()
+    url.pathname = '/'
+    return NextResponse.redirect(url)
+  }
+
+  if (isProtectedRoute(req) && !userId) {
+    const url = req.nextUrl.clone()
+    url.pathname = '/'
+    return NextResponse.redirect(url)
+  }
+})
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
-    '/(lists)(.*)',
-    
+    '/(lists|email|invoices)(.*)',
+    '/', // or '/(.*)' to match all routes including the root
   ],
 }
