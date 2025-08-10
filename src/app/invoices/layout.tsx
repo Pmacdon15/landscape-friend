e11cdaed6930fec0ed21bd2e504fc40213c5c4b6
Patrick MacDonald <patrick@patmac.ca>
2025-08-10 15:34:55 -0600

    removed space

--- src/app/invoices/layout.tsx
+++ src/app/invoices/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Landscape Friend',
   description: 'Track and Invoice your lawn clients easily',
-  keywords: 'lawn care, lawn tracking, invoice lawn clients, lawn management, send quote, manage invoices, manage quotes ',
+  keywords: 'lawn care, lawn tracking, invoice lawn clients, lawn management, send quote, manage invoices, manage quotes',
 };
 
 export default function RootLayout({
