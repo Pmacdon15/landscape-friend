cf1c2609a16174089fa80d3718bcee8f8d1610d8
Patrick MacDonald <patrick@patmac.ca>
2025-08-17 14:02:11 -0600

    update css

--- src/components/ui/image-list/list-view/list-view-warapper.tsx
+++ src/components/ui/image-list/list-view/list-view-warapper.tsx
@@ -1,6 +1,6 @@
 export default function ListViewWrapper({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex flex-col  min-h-[300px] w-full overflow-y-auto h-[300px] bg-background rounded-md p-2 mt-2 ">
+        <div className="flex flex-col  min-h-[300px] w-full overflow-y-auto h-[300px] bg-background rounded-md p-2  ">
             {children}
         </div>
     );
