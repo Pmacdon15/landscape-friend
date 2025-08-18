e89dde41cd939edee4e2d92dc33899cf1afe85e7
Patrick MacDonald <patrick@patmac.ca>
2025-08-17 18:03:21 -0600

    start of manage quotes page

--- src/DAL/dal-stripe.ts
+++ src/DAL/dal-stripe.ts
@@ -119,3 +119,75 @@ export async function fetchInvoices(typesOfInvoices: string, page: number, searc
         throw new Error('Failed to fetch invoices');
     }
 }
+
+
+export async function fetchQuotes(typesOfQuotes: string, page: number, searchTerm: string): Promise<FetchQuotesResponse> {
+    const { isAdmin } = await isOrgAdmin();
+    if (!isAdmin) throw new Error("Not Admin");
+
+    const stripe = await getStripeInstance();
+    const pageSize = Number(process.env.PAGE_SIZE) || 10;
+
+    try {
+        let allInvoices: Stripe.Invoice[] = [];
+        let hasMore = true;
+        let startingAfter: string | undefined = undefined;
+
+        const params: Stripe.InvoiceListParams = { limit: 100 };
+        if (typesOfQuotes && ['draft', 'paid', 'open'].includes(typesOfQuotes)) {
+            params.status = typesOfQuotes as 'draft' | 'paid' | 'open';
+        }
+
+        while (hasMore) {
+            const invoiceBatch: Stripe.ApiList<Stripe.Invoice> = await stripe.invoices.list({ ...params, starting_after: startingAfter });
+            allInvoices = allInvoices.concat(invoiceBatch.data);
+            hasMore = invoiceBatch.has_more;
+            if (hasMore) {
+                startingAfter = invoiceBatch.data[invoiceBatch.data.length - 1].id;
+            }
+        }
+
+        let filteredInvoices = allInvoices;
+        if (searchTerm) {
+            const lowerCaseSearchTerm = searchTerm.toLowerCase();
+            filteredInvoices = allInvoices.filter(invoice =>
+                (invoice.customer_name && invoice.customer_name.toLowerCase().includes(lowerCaseSearchTerm)) ||
+                (invoice.customer_email && invoice.customer_email.toLowerCase().includes(lowerCaseSearchTerm))
+            );
+        }
+
+        const totalInvoices = filteredInvoices.length;
+        const totalPages = Math.ceil(totalInvoices / pageSize);
+        const offset = (page - 1) * pageSize;
+        const paginatedInvoices = filteredInvoices.slice(offset, offset + pageSize);
+
+        const strippedInvoices = paginatedInvoices.map((invoice) => ({
+            id: invoice.id,
+            object: invoice.object,
+            amount_due: invoice.amount_due,
+            amount_paid: invoice.amount_paid,
+            amount_remaining: invoice.amount_remaining,
+            created: invoice.created,
+            currency: invoice.currency,
+            customer: invoice.customer,
+            customer_email: invoice.customer_email,
+            customer_name: invoice.customer_name,
+            due_date: invoice.due_date,
+            hosted_invoice_url: invoice.hosted_invoice_url,
+            invoice_pdf: invoice.invoice_pdf,
+            number: invoice.number,
+            status: invoice.status,
+            total: invoice.total,
+            lines: {
+                data: invoice.lines.data.map((lineItem) => ({
+                    id: lineItem.id,
+                })),
+            },
+        }));
+
+        return { invoices: strippedInvoices as StripeInvoice[], totalPages };
+    } catch (error) {
+        console.error(error);
+        throw new Error('Failed to fetch invoices');
+    }
+}
---
+++ src/app/billing/manage/quotes/page.tsx
@@ -0,0 +1,26 @@
+import { fetchQuotes } from "@/DAL/dal-stripe";
+import SearchForm from "@/components/ui/client-list/search-form";
+import FormContainer from "@/components/ui/containers/form-container";
+import FormHeader from "@/components/ui/header/form-header";
+import { PaginationTabs } from "@/components/ui/pagination/pagination-tabs";
+import { parseClientListParams } from "@/lib/params";
+import { SearchParams } from "@/types/types-params";
+import { Suspense } from "react";
+
+export default async function Page({ searchParams }: { searchParams: Promise<SearchParams> }) {
+    const params = await searchParams
+    const { searchTermStatus, page, searchTerm } = parseClientListParams(params);
+    const { Quotes, totalPages } = await fetchQuotes(searchTermStatus, page, searchTerm)
+
+    return (
+        <FormContainer>
+            <FormHeader text={"Manage Quotes"} />
+            <Suspense>
+                <SearchForm variant="invoices" />
+            </Suspense>
+            <PaginationTabs path={"/billing/manage/quotes"} page={page} totalPages={totalPages} fullWidth />
+            {/* <CardView invoices={invoices} /> */}
+            <PaginationTabs path={"/billing/manage/quotes"} page={page} totalPages={totalPages} fullWidth />
+        </FormContainer>
+    );
+}
