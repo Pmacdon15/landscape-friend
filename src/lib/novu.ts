3c612c52832b35346bfe3dc41cc37fbb5bf7e67a
Patrick MacDonald <patrick@patmac.ca>
2025-08-20 23:19:50 -0600

    removed unused statment

--- src/lib/novu.ts
+++ src/lib/novu.ts
@@ -2,11 +2,6 @@
 import { sayHello } from '@/DAL/dal-novu';
 import { Novu } from '@novu/api';
 
-const novu = new Novu({
-    secretKey: process.env.NOVU_SECRET_KEY as string,
-});
-
-
 export async function addNovuSubscriber(
     subscriberId: string,
     email?: string,
