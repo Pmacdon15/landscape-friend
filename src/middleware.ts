c0ac55496b3758f2314657391a1187a41f2a2f80
Patrick MacDonald <patrick@patmac.ca>
2025-08-24 16:04:08 -0600

    updated middle ware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 
 const isProtectedRoute = createRouteMatcher(['/lists(.*)', '/(email)(.*)', '/(settings)(.*)', '/billing(.*)'])
-const isAdminRoute = createRouteMatcher(['/invoices(.*)'])
+const isAdminRoute = createRouteMatcher(['/invoices(.*)', '/quotes(.*)'])
 //TODO: finish this routes
 export default clerkMiddleware(async (auth, req) => {
   // const clerk = await clerkClient()
