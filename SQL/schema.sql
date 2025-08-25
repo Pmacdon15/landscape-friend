e30658d85dc60e282c2d5d81b9ef1f7f382bb6ce
Patrick MacDonald <patrick@patmac.ca>
2025-08-24 22:32:35 -0600

    new schema

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -31,7 +31,7 @@ CREATE TABLE organizations (
     id SERIAL PRIMARY KEY,
     organization_id VARCHAR(253) NOT NULL UNIQUE,
     organization_name VARCHAR(253) NOT NULL,
-    max_allowed_clinents INT NOT NULL DEFAULT 50
+    max_allowed_clients INT NOT NULL DEFAULT 50
 );
 
 CREATE TABLE clients (
@@ -40,7 +40,7 @@ CREATE TABLE clients (
     phone_number VARCHAR(50) NOT NULL,
     email_address VARCHAR(75) NOT NULL,
     organization_id VARCHAR(253) NOT NULL,
-    FOREIGN KEY (organization_id) REFERENCES organizations (organization_id),
+    FOREIGN KEY (organization_id) REFERENCES organizations (organization_id) ON DELETE CASCADE,
     price_per_cut FLOAT NOT NULL DEFAULT 51.5,
     address VARCHAR(200) NOT NULL,
     snow_client BOOLEAN NOT NULL DEFAULT false,
@@ -52,15 +52,14 @@ CREATE TABLE stripe_api_keys (
     id SERIAL PRIMARY KEY,
     api_key VARCHAR(253) NOT NULL,
     organization_id VARCHAR(253) NOT NULL,
-    FOREIGN KEY (organization_id) REFERENCES organizations (organization_id)
-    -- CONSTRAINT unique_organization_id UNIQUE (organization_id)
+    FOREIGN KEY (organization_id) REFERENCES organizations (organization_id) ON DELETE CASCADE
 );
 
 CREATE TABLE accounts (
     id SERIAL PRIMARY KEY,
     client_id INT NOT NULL,
     current_balance FLOAT NOT NULL DEFAULT 0.0,
-    FOREIGN KEY (client_id) REFERENCES clients (id)
+    FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE CASCADE
 );
 
 CREATE TABLE payments (
@@ -68,8 +67,8 @@ CREATE TABLE payments (
     account_id INT NOT NULL,
     amount FLOAT NOT NULL,
     organization_id VARCHAR(253) NOT NULL,
-    FOREIGN KEY (organization_id) REFERENCES organizations (organization_id),
-    FOREIGN KEY (account_id) REFERENCES accounts (id)
+    FOREIGN KEY (organization_id) REFERENCES organizations (organization_id) ON DELETE CASCADE,
+    FOREIGN KEY (account_id) REFERENCES accounts (id) ON DELETE CASCADE
 );
 
 CREATE TABLE cutting_schedule (
@@ -77,7 +76,7 @@ CREATE TABLE cutting_schedule (
     cutting_week INT NOT NULL,
     cutting_day VARCHAR(10) NOT NULL,
     client_id INT NOT NULL,
-    FOREIGN KEY (client_id) REFERENCES clients (id),
+    FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE CASCADE,
     UNIQUE (client_id, cutting_week)
 );
 
@@ -85,7 +84,7 @@ CREATE TABLE yards_marked_cut (
     id SERIAL PRIMARY KEY,
     cutting_date DATE NOT NULL,
     client_id INT NOT NULL,
-    FOREIGN KEY (client_id) REFERENCES clients (id),
+    FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE CASCADE,
     UNIQUE (client_id, cutting_date)
 );
 
@@ -93,7 +92,7 @@ CREATE TABLE yards_marked_clear (
     id SERIAL PRIMARY KEY,
     clearing_date DATE NOT NULL,
     client_id INT NOT NULL,
-    FOREIGN KEY (client_id) REFERENCES clients (id),
+    FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE CASCADE,
     UNIQUE (client_id, clearing_date)
 );
 
@@ -102,8 +101,8 @@ CREATE TABLE snow_clearing_assignments (
     client_id INT NOT NULL,
     assigned_to VARCHAR(75) NOT NULL,
     organization_id VARCHAR(253) NOT NULL,
-    FOREIGN KEY (client_id) REFERENCES clients (id),
-    FOREIGN KEY (organization_id) REFERENCES organizations (organization_id),
+    FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE CASCADE,
+    FOREIGN KEY (organization_id) REFERENCES organizations (organization_id) ON DELETE CASCADE,
     UNIQUE (client_id)
 );
 
@@ -114,14 +113,12 @@ CREATE TABLE IF NOT EXISTS images (
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     isActive BOOLEAN
 );
-
-
 -- SELECT * FROM images;
 -- SELECT * FROM yards_marked_cut;
 -- SELECT * FROM yards_um that might not work masybe marked_clear;
 -- SELECT * FROM cutting_schedule;
 -- SELECT * FROM clients;
-SELECT * FROM users;
+-- SELECT * FROM users;
 
 -- SELECT id, novu_subscriber_id 
 --             FROM users 
