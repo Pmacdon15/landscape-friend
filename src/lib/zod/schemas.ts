9a9f7410e58dfd4b8c172f8314bafcb56de56da8
Patrick MacDonald <patrick@patmac.ca>
2025-08-24 22:57:57 -0600

    now allowing larger file sizes

--- src/lib/zod/schemas.ts
+++ src/lib/zod/schemas.ts
@@ -82,7 +82,7 @@ export const AddClientSchema = z.object({
     lat: z.number().optional(),
     lng: z.number().optional(),
 });
-export const MAX_FILE_SIZE = 500000;
+export const MAX_FILE_SIZE = 5 * 1024 * 1024;
 export const ACCEPTED_IMAGE_TYPES = [
     "image/jpeg",
     "image/jpg",
