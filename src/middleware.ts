2dcf0609263ebc5b06d223fbed88b0a5477d271a
Patrick MacDonald <patrick@patmac.ca>
2025-08-31 16:38:36 -0600

    updated midlleware with out clerk client

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkClient, clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 
 const isProtectedRoute = createRouteMatcher(['/lists(.*)', '/(email)(.*)', '/(settings)(.*)', '/billing(.*)'])
@@ -6,20 +6,20 @@ const isAdminRoute = createRouteMatcher(['/invoices(.*)', '/quotes(.*)'])
 //TODO: finish this routes
 export default clerkMiddleware(async (auth, req) => {
   // const clerk = await clerkClient()
-  // const { userId, orgId, sessionClaims } = await auth()
-  // // console.log("Admin?: ", sessionClaims?.orgRole)
+  const { userId, orgId, sessionClaims } = await auth()
+  // console.log("Admin?: ", sessionClaims?.orgRole)
 
-  // if (isProtectedRoute(req) && !userId) {
-  //   const url = req.nextUrl.clone()
-  //   url.pathname = '/'
-  //   return NextResponse.redirect(url)
-  // }
+  if (isProtectedRoute(req) && !userId) {
+    const url = req.nextUrl.clone()
+    url.pathname = '/'
+    return NextResponse.redirect(url)
+  }
 
-  // if (isAdminRoute(req) && sessionClaims?.orgRole !== 'org:admin' && orgId) {
-  //   const url = req.nextUrl.clone()
-  //   url.pathname = '/'
-  //   return NextResponse.redirect(url)
-  // }
+  if (isAdminRoute(req) && sessionClaims?.orgRole !== 'org:admin' && orgId) {
+    const url = req.nextUrl.clone()
+    url.pathname = '/'
+    return NextResponse.redirect(url)
+  }
 
 })
 export const config = {
