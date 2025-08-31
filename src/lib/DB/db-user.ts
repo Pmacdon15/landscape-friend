f9e408b4aba965cbdc293a8da46cd2c635f78d37
Patrick MacDonald <patrick@patmac.ca>
2025-08-31 12:46:18 -0600

    abstract function

---
+++ src/lib/DB/db-user.ts
@@ -0,0 +1,12 @@
+
+import { neon } from "@neondatabase/serverless";
+import { UserNovuId } from "@/types/types-novu";
+
+export async function fetchUniqueIdDb(userId: string): Promise<UserNovuId | null> {
+  const sql = neon(`${process.env.DATABASE_URL}`);
+  const result = await sql`SELECT novu_subscriber_id FROM users WHERE id = ${userId}`;
+  if (result.length > 0) {
+    return { UserNovuId: result[0].novu_subscriber_id };
+  }
+  return null;
+}
--- src/lib/dal/user-dal.ts
+++ src/lib/dal/user-dal.ts
@@ -1,14 +1,8 @@
 import { UserNovuId } from "@/types/types-novu";
 import { auth } from "@clerk/nextjs/server";
-import { neon } from "@neondatabase/serverless";
+import { fetchUniqueIdDb } from "../DB/db-user";
 
-export async function fetchNovuId(userId: string):
-    Promise<UserNovuId | null> {
-    await auth.protect()
-    const sql = neon(`${process.env.DATABASE_URL} `);
-    const result = await sql` SELECT novu_subscriber_id FROM users where id = ${userId} `;
-    if (result.length > 0) {
-        return { UserNovuId: result[0].novu_subscriber_id };
-    }
-    return null;
+export async function fetchNovuId(userId: string): Promise<UserNovuId | null> {
+    await auth.protect();
+    return await fetchUniqueIdDb(userId);
 }
