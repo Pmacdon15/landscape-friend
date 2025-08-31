3ce229239e59f0222adc27c1ae7e90968ef327ac
Patrick MacDonald <patrick@patmac.ca>
2025-08-31 16:46:08 -0600

    updated matched to exclude api routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -22,8 +22,7 @@ export default clerkMiddleware(async (auth, req) => {
   }
 
 })
+
 export const config = {
-  matcher: [
-    '/((?!_next|static|favicon.ico|api/webhooks$).*)',
-  ],
+  matcher: ['/((?!_next|static|favicon.ico).*)'],
 }
