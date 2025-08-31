e1e9a7497ed5bb5fd6d9eb5a807c5a50eb684397
Patrick MacDonald <patrick@patmac.ca>
2025-08-31 17:00:41 -0600

    fixed unsaved file

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,27 +5,35 @@ const isProtectedRoute = createRouteMatcher(['/lists(.*)', '/(email)(.*)', '/(se
 const isAdminRoute = createRouteMatcher(['/invoices(.*)', '/quotes(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
+  console.log("middleware Start")
   const { userId, orgId, sessionClaims } = await auth()
   // console.log("Admin?: ", sessionClaims?.orgRole)
 
   if (isProtectedRoute(req) && !userId) {
     const url = req.nextUrl.clone()
     url.pathname = '/'
+    console.log("Calling redirect from middleware")
     return NextResponse.redirect(url)
   }
 
   if (isAdminRoute(req) && sessionClaims?.orgRole !== 'org:admin' && orgId) {
     const url = req.nextUrl.clone()
     url.pathname = '/'
+    console.log("Calling redirect from middleware")
     return NextResponse.redirect(url)
   }
+  console.log("middleware End")
 })
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes, except for webhooks
-    '/(api(?!/webhooks)|trpc)(.*)',
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api/webhooks (webhook routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api/webhooks|_next/static|_next/image|favicon.ico).*)',
   ],
 }
