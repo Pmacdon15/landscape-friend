f34002facfdc045eae1f4a390dab2611d4f52827
Patrick MacDonald <patrick@patmac.ca>
2025-09-01 15:37:07 -0600

    fixed nav bar

--- src/components/ui/nav/vertical-nav.tsx
+++ src/components/ui/nav/vertical-nav.tsx
@@ -28,7 +28,7 @@ export default function VerticalNav({ userId }: { userId: string }) {
 
     return (
         <nav className="flex flex-col gap-2 w-full">
-            <div className="flex flex-col justify-center items-center p-4 w-full">
+            <div className="flex flex-col justify-center items-center md:p-4 w-full">
                 <Image src="/logo.png" alt="Lawn Buddy Logo" width={100} height={100} />
                 <HeaderTitle text='Landscape Friend' />
             </div>
