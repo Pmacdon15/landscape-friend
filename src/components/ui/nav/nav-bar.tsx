e66da07158ea534a84b53df546a51782d486a6dc
Patrick MacDonald <patrick@patmac.ca>
2025-09-01 15:03:37 -0600

    stage 3

--- src/components/ui/nav/nav-bar.tsx
+++ src/components/ui/nav/nav-bar.tsx
@@ -1,26 +1,30 @@
-"use client"
+'use client'
 import * as React from "react"
 import { Button } from "../button"
-import { Suspense, useState } from "react"
+import { Suspense } from "react"
 import { Menu } from "lucide-react"
 import NavigationMenuComponent from "./nav-menu-component"
 import VerticalNav from "./vertical-nav"
+import { Sheet, SheetContent, SheetTitle, SheetTrigger } from "@/components/ui/sheet"
 
 export function NavBar({ userId }: { userId: string }) {
-
-    const [showNav, setShowNav] = useState(false)
-
     return (
         <>
-            <div className="md:hidden flex flex-col gap-2 pb-2">
-                <Button variant="outline" onClick={() => setShowNav(!showNav)} className="p-2 w-9">
-                    <Menu size={24} />
-                </Button>
-                {showNav && <Suspense><VerticalNav userId={userId} /></Suspense>}
+            <div className="md:hidden">
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button variant="outline" className="p-2 w-9">
+                            <Menu size={24} />
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent side="left">
+                        <SheetTitle className="sr-only">Navigation Menu</SheetTitle>
+                        <Suspense><VerticalNav userId={userId} /></Suspense>
+                    </SheetContent>
+                </Sheet>
             </div>
 
             <div className="hidden md:block"><Suspense><NavigationMenuComponent userId={userId} /></Suspense></div>
         </>
     )
 }
-
