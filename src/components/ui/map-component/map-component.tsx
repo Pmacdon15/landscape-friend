3f053d6f4e0a6094feb977055437b6b458abd342
Patrick MacDonald <patrick@patmac.ca>
2025-09-17 22:55:37 -0600

    test

--- src/components/ui/map-component/map-component.tsx
+++ src/components/ui/map-component/map-component.tsx
@@ -1,23 +1,25 @@
 import { fetchGeocode } from "@/lib/utils/geocode";
 
+
 export default async function MapComponent({ address }: { address: string }) {
-  const result = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_MAPS_API_KEY}`)
-    .then(response => response.json());
+    // const result = await fetchGeocode(address);
 
-  if (result.error_message || !result.results || !result.results[0]) {
-    return <div>Error: Unable to fetch geocode data</div>;
-  }
+    // if (result.error) {
+    //     return <div>Error: {result.error}</div>;
+    // }
 
-  const { lat, lng } = result.results[0].geometry.location;
-  const mapUrl = `https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_MAPS_API_KEY}&q=${lat},${lng}&zoom=15`;
+    // const { coordinates, zoom } = result;
 
-  return (
-    <div className="flex flex-col md:flex-row w-full bg-background max-h-[300px] rounded-md p-2 ">
-      <iframe
-        title="Map View"
-        src={mapUrl}
-        className="w-full h-full"
-      />
-    </div>
-  );
+    const mapUrl = `https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_MAPS_API_KEY}&q=51.070028,-114.156139&zoom=${zoom}`;
+
+    return (
+        <div className="flex flex-col md:flex-row w-full bg-background max-h-[300px] rounded-md p-2 ">
+            <iframe
+                title="Map View"
+                src={mapUrl}
+                className="w-full h-full"
+            />
+        </div>
+    );
 }
+
