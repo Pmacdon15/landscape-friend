15a8c82d766e85b542d09edc3dedd3eedac0da3a
Patrick MacDonald <patrick@patmac.ca>
2025-10-24 22:25:47 -0600

    rename

--- src/components/ui/fallbacks/search/search-form-fallback.tsx
+++ src/components/ui/fallbacks/search/search-form-fallback.tsx
@@ -3,8 +3,8 @@ import type { SearchFormVariant } from '@/types/search-fallback-types'
 import { BillingStatusSelectorFallback } from './billing-status-selector-fallback'
 import { CuttingPeriodSelectorFallback } from './cutting-period-selector-fallback'
 import { SearchInputFallback } from './search-input-fallback'
+import { ServiceListDatePickerFallback } from './service-list-date-picker-fallback'
 import { ServiceStatusSelectorFallback } from './service-status-selector'
-import { ServiceListDatePickerFallback } from './servicel-list-date-picker-fallback'
 
 export default function SearchFormFallBack({
 	variant = 'default',
