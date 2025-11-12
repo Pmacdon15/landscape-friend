f38d9019f758de3c765278654dd752a801f917f2
Patrick MacDonald <patrick@patmac.ca>
2025-11-11 23:49:25 -0700

    add use opt and use tran to AssignedToSelector

--- src/components/ui/selectors/assigned-to-selector.tsx
+++ src/components/ui/selectors/assigned-to-selector.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { use } from 'react'
+import { use, useOptimistic, useTransition } from 'react'
 import { useSearchParam } from '@/lib/hooks/hooks'
 import type { OrgMember } from '@/types/clerk-types'
 
@@ -11,14 +11,27 @@ export function AssignedToSelector({
 }) {
 	const orgMembers = orgMembersPromise ? use(orgMembersPromise) : []
 	const { currentValue, setParam } = useSearchParam('assigned', '')
+	const [optimisticValue, setOptimisticValue] = useOptimistic(
+		currentValue,
+		(_, newValue: string) => newValue,
+	)
+	const [, startTransition] = useTransition()
+
+	function handleChange(e: React.ChangeEvent<HTMLSelectElement>) {
+		const newValue = e.target.value
+		startTransition(() => {
+			setOptimisticValue(newValue)
+			setParam(newValue)
+		})
+	}
 
 	return (
 		<div className="flex gap-1">
 			<select
 				className="w-fit rounded-sm border py-2 text-center"
 				name={'assigned_to'}
-				onChange={(e) => setParam(e.target.value)}
-				value={currentValue}
+				onChange={handleChange}
+				value={optimisticValue}
 			>
 				<option value="">Assigned To</option>
 				{orgMembers.map((orgMember) => (
