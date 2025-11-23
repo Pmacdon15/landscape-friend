e22d887b03dc9ae514ffcfd7805d2c3cdaa49c40
Patrick MacDonald <patrick@patmac.ca>
2025-11-23 12:37:40 -0700

    undo logs

--- src/lib/dal/dal-org.ts
+++ src/lib/dal/dal-org.ts
@@ -23,7 +23,7 @@ export async function fetchOrgMembers(): Promise<OrgMember[]> {
 
 		// Transform OrganizationMembership objects into the simplified OrgMember type
 		const orgMembers: OrgMember[] = response.data.flatMap((member) => {
-			console.log('Org Members: ', orgMembers)
+			// console.log('Org Members: ', orgMembers)
 			const userId = member.publicUserData?.userId
 			if (!userId) {
 				return [] // Skip this member if userId is not available
