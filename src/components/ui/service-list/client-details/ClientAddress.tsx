869cfb71c581329356f4fbfcf7471b1658384786
Patrick MacDonald <patrick@patmac.ca>
2025-11-24 00:39:28 -0700

    added clickable map

--- src/components/ui/service-list/client-details/ClientAddress.tsx
+++ src/components/ui/service-list/client-details/ClientAddress.tsx
@@ -1,22 +1,30 @@
 import Image from 'next/image'
 import type React from 'react'
+import { Activity, useState } from 'react'
+import MapComponent from '../../map-component/map-component'
 
 interface ClientAddressProps {
 	address: string
 }
 
 const ClientAddress: React.FC<ClientAddressProps> = ({ address }) => {
+	const [isShowing, setIsShowing] = useState(false)
 	return (
-		<div className="flex w-full items-center justify-center gap-2">
-			<Image
-				alt="Address Icon"
-				className="h-8 w-8"
-				height={512}
-				src="/client-list/address.png"
-				width={512}
-			/>
-			<p>Address: {address}</p>
-		</div>
+		<>
+			<button className="flex w-full items-center justify-center gap-2" onClick={() => setIsShowing(!isShowing)} type='button'>
+				<Image
+					alt="Address Icon"
+					className="h-8 w-8"
+					height={512}
+					src="/client-list/address.png"
+					width={512}
+				/>
+				<p>Address: {address}</p>
+			</button>
+			<Activity mode={isShowing ? 'visible' : 'hidden'}>
+				<MapComponent address={address} />
+			</Activity>
+		</>
 	)
 }
 
