18d7a10707ecf902d0626b0630f166451673b025
Patrick MacDonald <patrick@patmac.ca>
2025-12-02 20:21:04 -0700

    change logic

--- src/components/ui/search/search-form.tsx
+++ src/components/ui/search/search-form.tsx
@@ -27,7 +27,7 @@ export default async function SearchForm({
 				variant !== 'quotes' &&
 				variant !== 'subscriptions' && (
 					<Suspense fallback={<AssignedToSelectorFallback />}>
-						{!isAdmin && (
+						{isAdmin && (
 							<AssignedToSelector
 								orgMembersPromise={orgMembersPromise}
 							/>
