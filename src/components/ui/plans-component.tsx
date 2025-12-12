a79987fdbc53c4876441815f5fe277d3f3aa47a8
Patrick MacDonald <patrick@patmac.ca>
2025-12-11 17:17:07 -0700

    use suspense to remove error

--- src/components/ui/plans-component.tsx
+++ src/components/ui/plans-component.tsx
@@ -4,6 +4,7 @@ import {
 	SignInButton,
 	SignUpButton,
 } from '@clerk/nextjs'
+import { Suspense } from 'react'
 import FillFormContainer from './containers/fill-form-container'
 import FormContainer from './containers/form-container'
 import FormHeader from './header/form-header'
@@ -18,20 +19,22 @@ export default function PlansComponent() {
 					<PricingTable for="organization" />
 				</div>
 				<div className="flex flex-col items-center sm:flex-row">
-					<SignedOut>
-						<h1 className="text-muted-foreground">
-							Sign in and create an organization to start free
-							trail!
-						</h1>
-						<div className="ml-auto flex gap-4 rounded-sm bg-white/30 p-2 backdrop-blur-md backdrop-filter">
-							<SignInButton
-								forceRedirectUrl={'/documentation/plans'}
-							/>
-							<SignUpButton
-								forceRedirectUrl={'/documentation/plans'}
-							/>
-						</div>
-					</SignedOut>
+					<Suspense>
+						<SignedOut>
+							<h1 className="text-muted-foreground">
+								Sign in and create an organization to start free
+								trail!
+							</h1>
+							<div className="ml-auto flex gap-4 rounded-sm bg-white/30 p-2 backdrop-blur-md backdrop-filter">
+								<SignInButton
+									forceRedirectUrl={'/documentation/plans'}
+								/>
+								<SignUpButton
+									forceRedirectUrl={'/documentation/plans'}
+								/>
+							</div>
+						</SignedOut>
+					</Suspense>
 				</div>
 			</FillFormContainer>
 		</FormContainer>
