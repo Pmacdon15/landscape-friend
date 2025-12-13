0760f7c4d48129b7056b4058a647b787bc77b3f4
Patrick MacDonald <patrick@patmac.ca>
2025-12-12 19:17:46 -0700

    added back home button

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,14 @@
 import NarrowFormContainer from '@/components/ui/containers/narrow-form-container'
 import { ReachOutForm } from '@/components/ui/forms/forms-reach-out/reach-out-form'
+import BackToLink from '@/components/ui/links/back-to-link'
 
 export default function Page() {
 	return (
 		<NarrowFormContainer>
 			<ReachOutForm />
+			<div className="mx-auto">
+				<BackToLink path={'/'} place={'Home'} />
+			</div>
 		</NarrowFormContainer>
 	)
 }
